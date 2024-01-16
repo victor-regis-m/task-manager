79ff9507ff283c90d0590a93afc0232d821982f8
Victor <victor.regism@gmail.com>
2024-01-15 22:51:26 -0300

    Created base mongoose schema and validations

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,9 +1,12 @@
+const Task = require('../models/task')
+
 const getAllTasks = (req, res) => {
     res.send('get all tasks')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body).catch((err)=> res.status(422).send(err.message));
+    res.status(201).json({task});
 }
 
 const getTask = (req, res) => {
